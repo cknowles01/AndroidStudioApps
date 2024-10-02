1a5d37fc5234a74d2ac2ae35e8ac984890dd5722
Connor <resistingprawn5@gmail.com>
2024-10-01 23:18:29 -0600

    a little plain but images look nice and both text fields are all done. could turn this in if i had to

--- Gallery/app/src/main/java/com/example/gallery/MainActivity.kt
+++ Gallery/app/src/main/java/com/example/gallery/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -19,8 +20,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
 
 
@@ -37,6 +40,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -91,13 +96,19 @@ fun ArtGallery(modifier: Modifier = Modifier) {
             ) {
                 Button(onClick = {
                     if (currentPicture > 1) currentPicture--
-                }) {
-                    Text(text = "backward")
+                },
+                    modifier = Modifier
+                        .width(120.dp)
+                ) {
+                    Text(text = "Previous")
                 }
                 Button(onClick = {
                     if (currentPicture < 8) currentPicture++
-                }) {
-                    Text(text = "forward")
+                },
+                    modifier = Modifier
+                        .width(120.dp)
+                ) {
+                    Text(text = "Next")
                     Log.d("ArtGaller", "currentPicture = $currentPicture")
                 }
             }
@@ -108,21 +119,88 @@ fun ArtGallery(modifier: Modifier = Modifier) {
 
 @Composable
 fun ChangePicture(pictureNumber: Int, modifier: Modifier = Modifier) {
-    val imageResource = when (pictureNumber) {
-        1 -> { R.drawable.crossing }
-        2 -> { R.drawable.johannes_vermeer__1632_1675____the_girl_with_the_pearl_earring__1665_}
-        3 -> { R.drawable.katsushikahokusaipaint_1600x_jpg}
-        4 -> { R.drawable.mona_lisa__by_leonardo_da_vinci__from_c2rmf_retouched}
-        5 -> { R.drawable.self_portrait_with_straw_hat_1887_88_857x1024}
-        6 -> { R.drawable.the_persistence_of_memory}
-        7 -> { R.drawable.the_scream}
-        8 -> { R.drawable.the_starry_night_by_vincent_van_gogh_1889_800x}
+    var imageResource = R.drawable.error
+    var titleResource = 1
+    var infoResource = 1
+
+    when (pictureNumber) {
+        1 -> {
+            imageResource = R.drawable.crossing
+            titleResource = R.string.washingtonName
+            infoResource = R.string.washingtonInfo
+        }
+        2 -> {
+            imageResource = R.drawable.johannes_vermeer__1632_1675____the_girl_with_the_pearl_earring__1665_
+            titleResource = R.string.pearlName
+            infoResource = R.string.pearlInfo}
+        3 -> {
+            imageResource = R.drawable.katsushikahokusaipaint_1600x_jpg
+            titleResource = R.string.kanagawaName
+            infoResource = R.string.kanagawaInfo}
+        4 -> {
+            imageResource = R.drawable.mona_lisa__by_leonardo_da_vinci__from_c2rmf_retouched
+            titleResource = R.string.monalisaName
+            infoResource = R.string.monalisaInfo}
+        5 -> {
+            imageResource = R.drawable.self_portrait_with_straw_hat_1887_88_857x1024
+            titleResource = R.string.hatName
+            infoResource = R.string.hatInfo}
+        6 -> {
+            imageResource = R.drawable.the_persistence_of_memory
+            titleResource = R.string.memoryName
+            infoResource = R.string.memoryInfo}
+        7 -> {
+            imageResource = R.drawable.the_scream
+            titleResource = R.string.screamName
+            infoResource = R.string.screamInfo}
+        8 -> {
+            imageResource = R.drawable.the_starry_night_by_vincent_van_gogh_1889_800x
+            titleResource = R.string.starrynightName
+            infoResource = R.string.starrynightInfo}
         else -> { R.drawable.error}
     }
-    Image(
-        painter = painterResource(id = imageResource),
-        contentDescription = null,
-        modifier = modifier.fillMaxWidth().aspectRatio(1f), //this will maintain the aspect ration of image
-        contentScale = ContentScale.Fit //scales to fit image in the box
-    )
+
+
+    Column (
+        modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+        ) {
+
+        Box(modifier = Modifier
+            .fillMaxWidth(),
+            //.weight(.1f),
+            
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = null,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f), //this will maintain the aspect ration of image
+                contentScale = ContentScale.Fit //scales to fit image in the box
+            )
+        }
+        Text(
+            text = stringResource(id = titleResource),
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+
+        )
+        Text(
+            text = stringResource(id = infoResource),
+            style = MaterialTheme.typography.bodySmall,
+            modifier = Modifier
+                //.padding(top = 8.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+
+        )
+
+    }
 }
