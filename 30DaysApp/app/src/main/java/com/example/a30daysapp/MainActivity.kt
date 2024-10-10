8d5e69e06d31fc7a418d82f21e0f5ac8029de4ba
Connor <resistingprawn5@gmail.com>
2024-10-10 17:28:11 -0600

    30daysapp currently runs though it looks like shit

--- 30DaysApp/app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ 30DaysApp/app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -18,6 +22,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a30daysapp.data.Tip
+import com.example.a30daysapp.data.tips
 import com.example.a30daysapp.ui.theme._30DaysAppTheme
 import org.jetbrains.annotations.Contract
 
@@ -28,14 +33,30 @@ class MainActivity : ComponentActivity() {
         setContent {
             _30DaysAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    ThirtyDaysApp(
+                        
                     )
                 }
             }
         }
     }
+}
+
+@Composable
+fun ThirtyDaysApp() {
+    Scaffold (
+
+    ) {
+        LazyColumn (contentPadding = it) {
+            items(tips) {
+                QuoteCard(
+                    tip = it,
+                    modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_24))
+                )
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -45,6 +66,13 @@ fun QuoteCard(
     ) {
     Card(modifier = modifier) {
         Column() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(R.dimen.padding_16))
+            ) {
+                TipInformation(day = tip.day, name = tip.tipname, quote = tip.quote)
+            }
 
         }
     }
@@ -66,6 +94,24 @@ fun TipInformation(
             style = MaterialTheme.typography.displayMedium,
             modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_8))
         )
+        Text(
+            text = stringResource(name),
+            style = MaterialTheme.typography.displayLarge,
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_16))
+        )
+        Text(
+            text = stringResource(quote),
+            style = MaterialTheme.typography.displaySmall,
+            modifier = Modifier.padding(top = dimensionResource(R.dimen.padding_24))
+        )
     }
 
+}
+
+
+
+@Preview
+@Composable
+fun ThirtyDaysPreview() {
+    ThirtyDaysApp()
 }
