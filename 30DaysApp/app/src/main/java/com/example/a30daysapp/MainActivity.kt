986b06874dcb920ba270213fb3c172746473764e
Connor <resistingprawn5@gmail.com>
2024-10-14 15:30:02 -0600

    working on adding animations'

--- 30DaysApp/app/src/main/java/com/example/a30daysapp/MainActivity.kt
+++ 30DaysApp/app/src/main/java/com/example/a30daysapp/MainActivity.kt
@@ -1,10 +1,26 @@
 package com.example.a30daysapp
 
 import android.os.Bundle
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.runtime.remember
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+
+
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,15 +29,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.a30daysapp.data.Tip
 import com.example.a30daysapp.data.tips
 
@@ -65,9 +89,27 @@ fun QuoteCard(
     tip: Tip,
     modifier: Modifier = Modifier,
     ) {
-    Card(modifier = modifier
-        .fillMaxWidth()
-        .wrapContentHeight()
+
+    var expanded by remember { mutableStateOf(false) }
+
+    val scale by animateFloatAsState(
+        targetValue = if (expanded) 1.15f else 1f,
+            animationSpec = tween(durationMillis = 500)
+
+    )
+
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable { expanded = !expanded }
+            .animateContentSize()
+            .graphicsLayer(scaleX = scale, scaleY = scale)
+            .padding(dimensionResource(id = R.dimen.padding_8)),
+            //.clip(RoundedCornerShape(dimensionResource(R.dimen.corner))),
+
+        shape = RoundedCornerShape(dimensionResource(id = R.dimen.corner)),
+        elevation = CardDefaults.cardElevation(dimensionResource(id = R.dimen.elevation))
 
 
     ) {
@@ -113,6 +155,10 @@ fun TipInformation(
     }
 
 }
+
+
+
+
 
 
 
