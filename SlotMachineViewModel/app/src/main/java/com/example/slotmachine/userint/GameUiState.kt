9cf5bfac95cc10e90f59b06e39f3aa2b678fe857
Connor <resistingprawn5@gmail.com>
2024-10-22 14:46:10 -0600

    starting slot machine with a viewModel to retain the app state during config changes

---
+++ SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameUiState.kt
@@ -0,0 +1,6 @@
+package com.example.slotmachine.userint
+
+data class GameUiState(
+    val money: Int = 0,
+    val isGameOver: Boolean = false
+)
---
+++ SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.slotmachine.userint
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    
+}
