2f67312eb52ca0d8c2993629682d110b7fdbfb74
Connor <resistingprawn5@gmail.com>
2024-10-22 17:43:18 -0600

    working on GameViewModel for slot machine

--- SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameUiState.kt
+++ SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameUiState.kt
@@ -1,6 +1,9 @@
 package com.example.slotmachine.userint
 
+import com.example.slotmachine.R
+
 data class GameUiState(
-    val money: Int = 0,
+    var money: Int = 1000,
+    val currentSlots: List<Int> = listOf(R.drawable.error, R.drawable.error, R.drawable.error),
     val isGameOver: Boolean = false
 )
--- SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameViewModel.kt
+++ SlotMachineViewModel/app/src/main/java/com/example/slotmachine/userint/GameViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.slotmachine.userint
 
 import androidx.lifecycle.ViewModel
+import com.example.slotmachine.slotOdds
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel : ViewModel() {
-    
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    private fun updateMoney(updatedBalance: Int) {
+        val currentBalance = _uiState.value.money
+        val newBalance = currentBalance + updatedBalance
+        if (newBalance < 0) {
+            _uiState.value = _uiState.value.copy(money = 0, isGameOver = true)
+        } else {
+            _uiState.value = _uiState.value.copy(money = updatedBalance)
+        }
+    }
+
+    fun spinSlots() {
+        val slot1 = slotOdds()
+        val slot2 = slotOdds()
+        val slot3 = slotOdds()
+        _uiState.value = _uiState.value.copy(currentSlots = listOf(slot1, slot2, slot3))
+        slotResults(slot1, slot2, slot3)
+    }
+
+    private fun slotResults(slot1: Int, slot2: Int, slot3: Int) {
+        if (slot1 == slot2 || slot1 == slot3 || slot2 == slot3) {
+            updateMoney(200)
+        } else if (slot1 == slot2 && slot2 == slot3) {
+            updateMoney(1000)
+        } else {
+            updateMoney(-200)
+        }
+    }
+
+    fun resetGame() {
+        _uiState.value = _uiState.value.copy(isGameOver = true)
+    }
+
+
+
+
+    fun gameOver() {
+        _uiState.value.money = 10000
+    }
 }
