7d4a0c4d5cf47eb98138f9aaecafba9ccfc92b21
Connor <resistingprawn5@gmail.com>
2024-11-05 21:00:47 -0700

    shit works pretty good

--- RecipesAttempt2/app/src/main/java/com/example/recipesattempt2/IngredientList.kt
+++ RecipesAttempt2/app/src/main/java/com/example/recipesattempt2/IngredientList.kt
@@ -2,9 +2,3 @@ package com.example.recipesattempt2
 
 import androidx.compose.runtime.Composable
 
-@Composable
-fun IngredientsList(
-    ingredients: List<String>,
-    selectedIngredients: List<String>,
-    onIngredientT
-)
