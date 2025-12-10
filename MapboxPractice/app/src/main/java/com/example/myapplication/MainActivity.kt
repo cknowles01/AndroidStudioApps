643d1a779f604c3064db531c5dc4b0b63db9942c
Connor <resistingprawn5@gmail.com>
2025-12-10 14:38:29 -0700

    put MapScreen in it's own composable

--- MapboxPractice/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MapboxPractice/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,55 +36,72 @@ class MainActivity : ComponentActivity() {
 
         //compose function that tells ui what to display
         setContent {
-            //creates a state object that will control the camera
-            val mapViewportState = rememberMapViewportState {
-                setCameraOptions {
-                    zoom(4.0)
-                    center(Point.fromLngLat(-113.9828, 46.8603))
-                    pitch(0.0)
-                    bearing(0.0)
-                }
-            }
+            MapScreen()
 
-            // Handles location permissions
-            var hasPermission by remember { mutableStateOf(false) }
-            // android context; needed for permissions. LocalContext helps access the andriod system objects
-            val context = LocalContext.current
+        }
+    }
+}
 
-            //creates a launcher for permission requests. Lambda runs when user responds to permission dialogue box
-            val launcher = rememberLauncherForActivityResult(
-                ActivityResultContracts.RequestMultiplePermissions()
-            ) { permissions ->
-                hasPermission = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
-            }
 
-            //coroutine for when composable first enters composition. Unit means it only runs once
-            LaunchedEffect(Unit) {
-                hasPermission = ContextCompat.checkSelfPermission(
-                    context,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED
 
-                //launch permission box if no permission granted
-                if (!hasPermission) {
-                    launcher.launch(arrayOf(
-                        Manifest.permission.ACCESS_FINE_LOCATION,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ))
-                }
-            }
 
-            MapboxMap(
-                Modifier.fillMaxSize(),
-                mapViewportState = mapViewportState
-            ) {
-                if (hasPermission) {
-                    MapEffect(Unit) { mapView ->
-                        mapView.location.updateSettings {
-                            enabled = true
-                            pulsingEnabled = true
-                        }
-                    }
+
+
+
+
+
+
+
+
+
+@Composable
+fun MapScreen() {
+    val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+            zoom(4.0)
+            center(Point.fromLngLat(-113.9828, 46.8603))
+            pitch(0.0)
+            bearing(0.0)
+        }
+    }
+
+    // Handles location permissions
+    var hasPermission by remember { mutableStateOf(false) }
+    // android context; needed for permissions. LocalContext helps access the andriod system objects
+    val context = LocalContext.current
+
+    //creates a launcher for permission requests. Lambda runs when user responds to permission dialogue box
+    val launcher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        hasPermission = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true
+    }
+
+    //coroutine for when composable first enters composition. Unit means it only runs once
+    LaunchedEffect(Unit) {
+        hasPermission = ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        //launch permission box if no permission granted
+        if (!hasPermission) {
+            launcher.launch(arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ))
+        }
+    }
+
+    MapboxMap(
+        Modifier.fillMaxSize(),
+        mapViewportState = mapViewportState
+    ) {
+        if (hasPermission) {
+            MapEffect(Unit) { mapView ->
+                mapView.location.updateSettings {
+                    enabled = true
+                    pulsingEnabled = true
                 }
             }
         }
